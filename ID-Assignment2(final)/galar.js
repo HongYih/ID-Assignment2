a5fc1a26769021cf6d0bd3e319a79c6e058be90e
HongYih <73088531+HongYih@users.noreply.github.com>
2021-01-11 00:40:04 +0800

    My final final commit
    i dont know why my earlier commits didnt work properly

---
+++ ID-Assignment2(final)/galar.js
@@ -0,0 +1,70 @@
+const pokedex = document.getElementById('pokedex');
+const cachedPokemon = {};
+
+const fetchPokemon = async () => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=809&limit=89`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const pokemon = data.results.map((data, index) => ({
+        name: data.name,
+        id: index + 810,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 810}.png`
+    }));
+
+    displayPokemon(pokemon);
+};
+
+const displayPokemon = (pokemon) => {
+    const pokemonHTMLString = pokemon
+        .map(
+            (pokeman) =>
+                `
+    <li class="card" onclick="selectPokemon(${pokeman.id})">
+        <img class="card-image" src="${pokeman.image}"/>
+        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+        </a>
+    </li>
+        `
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+const selectPokemon = async (id) => {
+    if (!cachedPokemon[id]) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        const res = await fetch(url);
+        const pokeman = await res.json();
+        cachedPokemon[id] = pokeman;
+        displayPokemanPopup(pokeman);
+    } else {
+        displayPokemanPopup(cachedPokemon[id]);
+    }
+};
+
+const displayPokemanPopup = (pokeman) => {
+    console.log(pokeman);
+    const type = pokeman.types.map((type) => type.type.name).join(', ');
+    const ability = pokeman.abilities.map((ability) => ability.ability.name).join(', ');
+    const htmlString = `
+        <div class="popup">
+            <button id="closeBtn" onclick="closePopup()">Close</button>
+            <div class="card">
+                <img class="card-image" src="${
+                    pokeman.sprites['front_default']
+                }"/>
+                <h2 class="card-title">${pokeman.name}</h2>
+                <p>Type: ${type} <br/> Height: ${pokeman.height} <br/> Weight: ${
+        pokeman.weight} <br/> Ability: ${ability}</p>
+            </div>
+        </div>`;
+    pokedex.innerHTML = htmlString + pokedex.innerHTML;
+};
+
+const closePopup = () => {
+    const popup = document.querySelector('.popup');
+    popup.parentElement.removeChild(popup);
+};
+
+
+fetchPokemon();
